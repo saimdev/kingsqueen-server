2465d6a5802cb53c255249cc07d3eb42ee9751d1
Mujtaba <macbook@Mohammad-Mujtaba.local>
2024-05-04 18:04:10 +0500

    Initial Chage

--- src/middlewares/validators/admin/product.js
+++ src/middlewares/validators/admin/product.js
@@ -1,24 +1,32 @@
-const {body} = require('express-validator');
+const Joi = require('joi');
 
-
-module.exports = ()=>{
+module.exports = () => {
     return [
-        body('title')
-        .notEmpty().withMessage('Title can not be empty!')
-        .isString().withMessage('Title must be string!')
-        .escape(),
-
-        body('description')
-        .isString().withMessage('Description must be string')
-        .escape(),
-
-        body('price')
-        .isNumeric().withMessage('Price must be numeric!'),
-
-        body('discount_price')
-        .isNumeric().withMessage('Discount Price must be numeric!'),
-
-        body('stock')
-        .isNumeric().withMessage('Stock must be numeric!'),
-    ]
-}
+        (req, res, next) => {
+            const schema = Joi.object({
+                title: Joi.string().required().messages({
+                    'string.base': 'Title must be a string!',
+                    'any.required': 'Title cannot be empty!'
+                }),
+                description: Joi.string().messages({
+                    'string.base': 'Description must be a string!'
+                }),
+                price: Joi.number().messages({
+                    'number.base': 'Price must be numeric!'
+                }),
+                discount_price: Joi.number().messages({
+                    'number.base': 'Discount Price must be numeric!'
+                }),
+                stock: Joi.number().messages({
+                    'number.base': 'Stock must be numeric!'
+                })
+            });
+            
+            const validationResult = schema.validate(req.body);
+            if (validationResult.error) {
+                return res.status(400).json({ error: validationResult.error.details[0].message });
+            }
+            next();
+        }
+    ];
+};
